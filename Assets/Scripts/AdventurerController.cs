081998213e6c436ff0441aac12bec3cc96e8a19c
montypyle <77437020+montypyle@users.noreply.github.com>
2021-02-09 15:58:48 +0000

    controller working

--- Assets/Scripts/AdventurerController.cs
+++ Assets/Scripts/AdventurerController.cs
@@ -1,18 +1,97 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
 
 public class AdventurerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float jumpSpeed;
+    public float horizontalSpeed = 10;
+
+    public LayerMask whatIsGround;
+    public Transform groundcheck;
+    private float groundRadius = 0.5f;
+    private bool grounded;
+
+    private bool jump;
+
+    bool facingRight = true;
+
+    private float hAxis;
+
+    private Rigidbody2D theRigidBody;
+
+    private Animator theAnimator;
+
+
     void Start()
     {
-        
+        jump = false;
+        grounded = false;
+
+        theRigidBody = GetComponent<Rigidbody2D>();
+        theAnimator = GetComponent<Animator>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        jump = Input.GetKeyDown(KeyCode.Space);
+
+
+        hAxis = Input.GetAxis("Horizontal");
+
+        theAnimator.SetFloat("hspeed", Mathf.Abs(hAxis));
+        Collider2D colliderWeCollidedWith = Physics2D.OverlapCircle(groundcheck.position, groundRadius, whatIsGround);
+
+        grounded = (bool)colliderWeCollidedWith;
+
+        theAnimator.SetBool("ground", grounded);
+
+        float yVelocity = theRigidBody.velocity.y;
+
+        theAnimator.SetFloat("vspeed", yVelocity);
+
+
+        if (grounded)
+        {
+            if ((hAxis > 0) && (facingRight == false))
+            {
+                Flip();
+            }
+            else if ((hAxis < 0) && (facingRight == true))
+            {
+                Flip();
+            }
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (grounded && !jump)
+        {
+
+            theRigidBody.velocity = new Vector2(horizontalSpeed * hAxis, theRigidBody.velocity.y);
+        }
+        else if (grounded && jump)
+        {
+
+            theRigidBody.velocity = new Vector2(theRigidBody.velocity.x, jumpSpeed);
+        }
+
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+
+        theScale.x *= -1;
+
+        transform.localScale = theScale;
     }
 }
